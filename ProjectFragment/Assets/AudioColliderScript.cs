1e5c51dbed418812efeaab4fdc8ad67ded923f3d
ckinne1771 <ckinney1771@gmail.com>
2015-05-15 19:54:34 +0100

    testing things

---
+++ ProjectFragment/Assets/AudioColliderScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioColliderScript : MonoBehaviour {
+	
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			Debug.Log("collision");
+			transform.gameObject.GetComponent<AudioSource>().Play();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		
+		if(other.tag == "Player")
+		{
+			Debug.Log("collision");
+			transform.gameObject.GetComponent<AudioSource>().Stop();
+		}
+}
+}
---
+++ ProjectFragment/Assets/PlayerMovementScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour {
+	public bool moveRight = false;
+	public bool moveLeft = false;
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.D))
+		{
+			Debug.Log ("entering if");
+			moveRight = true;
+		}
+		if(Input.GetKeyDown(KeyCode.A))
+		   {
+
+			moveLeft = true;
+		}
+		if(Input.GetKeyUp (KeyCode.D))
+		{
+			moveRight = false;
+		}
+		if(Input.GetKeyUp(KeyCode.A))
+		{
+			moveLeft = false;
+		}
+
+		if(moveLeft==true)
+		{
+			transform.Translate(-6f*Time.deltaTime, 0f, 0f);
+		}
+		if(moveRight==true)
+		{
+			transform.Translate (6f*Time.deltaTime, 0f, 0f);
+		}
+	
+	}
+}
